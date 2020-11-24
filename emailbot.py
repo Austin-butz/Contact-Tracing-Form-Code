d109eec51061d3f60a47d00fccc938a4e48dbbb7
Austin-butz <abutz@fandm.edu>
2020-11-24 12:57:45 -0500

    overhaul of driver

--- emailbot.py
+++ emailbot.py
@@ -1,25 +1,21 @@
-# Importing libraries 
 import imaplib, email 
 
 user = 'thisisatestforanemailbot@gmail.com'
 password = 'A123456&'
 imap_url = 'imap.gmail.com'
 
-# Function to get email content part i.e its body part 
 def get_body(msg): 
 	if msg.is_multipart(): 
 		return get_body(msg.get_payload(0)) 
 	else: 
 		return msg.get_payload(None, True) 
 
-# Function to search for a key value pair 
 def search(key, value, con): 
 	result, data = con.search(None, key, '"{}"'.format(value)) 
 	return data 
 
-# Function to get the list of emails under this label 
 def get_emails(result_bytes, con): 
-    msgs = [] # all the email data are pushed inside an array 
+    msgs = []
     for num in result_bytes[0].split(): 
         typ, data = con.fetch(num, '(RFC822)') 
         msgs.append(data)
@@ -30,37 +26,20 @@ def get_emails(result_bytes, con):
 
 def record(filename):
     outfile = open(filename, 'w+')
-# this is done to make SSL connnection with GMAIL 
     con = imaplib.IMAP4_SSL(imap_url) 
 
-# logging the user in 
     con.login(user, password) 
 
-# calling function to check for email under this label 
     con.select('Inbox') 
 
-# fetching emails from this user "tu**h*****1@gmail.com" 
     msgs = get_emails(search('FROM', 'donotreply@wordpress.com', con), con) 
 
-# Uncomment this to see what actually comes as data 
-# print(msgs) 
-
-
-# Finding the required content from our msgs 
-# User can make custom changes in this part to 
-# fetch the required content he / she needs 
-
-# printing them by the order they are displayed in your gmail 
     for msg in msgs[::-1]: 
         for sent in msg: 
             if type(sent) is tuple: 
-
-			# encoding set as utf-8 
                 content = str(sent[1], 'utf-8') 
                 data = str(content) 
 
-			# Handling errors related to unicodenecode 
-			#try: 
                 indexstart = data.find("ltr") 
                 data2 = data[indexstart + 5: len(data)] 
                 indexend = data2.find("</div>") 
@@ -71,7 +50,7 @@ def record(filename):
                 timestrt = int(body.find('Time: '))
                 timeend = int(body.find('IP Address: ')-2)
                 ipstrt = int(body.find('IP Address: '))
-                ipend = int(body.find('Contact Form URL: ')-4)
+                ipend = int(body.find('Contact Form URL: ')-1)
                 name = body[namestrt:emailstrt-4] + ' '
                 emailadd = body[emailstrt:numstrt-4] + ' '
                 phonenum = body[numstrt:timestrt-12] + ' '
--- emaildriver.py
+++ emaildriver.py
@@ -1,8 +1,56 @@
 import time, string, emailbot, os
 
-while (1):
-    fulltime = str(time.asctime(time.localtime(time.time())))
-    if '00:00:00' in fulltime:
-        print('Created New Contact Tracing List at: ')
-        print(fulltime)
-        emailbot.record(fulltime)
+print('Welcome, please select a time interval for recording contact tracing files')
+print('1: Hourly')
+print('2: Every 12 hours')
+print('3: Daily')
+print('q: Quit')
+input1 = input()
+
+while input1 not in ['1', '2', '3', '4', 'q', 'Q', 't', 'T']:#T = test
+    print('Invalid response, please type one of the following options')
+    print('1: Hourly')
+    print('2: Every 12 hours')
+    print('3: Daily')
+    print('q: Quit')
+    input1 = input()
+
+if input1 not in ['q', 'Q', 't', 'T']:
+    print('Running, press CTRL+Z to stop')
+
+if input1 == '1':
+    while (1):
+        fulltime = str(time.asctime(time.localtime(time.time())))
+        if ':00:00 ' in fulltime:
+            print('Created New Contact Tracing List:', fulltime)
+            emailbot.record(fulltime)
+            time.sleep(1)
+
+elif input1 == '2':
+    while (1):
+        fulltime = str(time.asctime(time.localtime(time.time())))
+        if '12:00:00' in fulltime or '00:00:00' in fulltime:
+            print('Created New Contact Tracing List:', fulltime)
+            emailbot.record(fulltime)
+            time.sleep(1)
+
+elif input1 == '3':
+    while (1):
+        fulltime = str(time.asctime(time.localtime(time.time())))
+        if '00:00:00' in fulltime:
+            print('Created New Contact Tracing List:', fulltime)
+            emailbot.record(fulltime)
+            time.sleep(1)
+
+elif input1 == 't' or 'T':
+    while (1):
+        fulltime = str(time.asctime(time.localtime(time.time())))
+        if '12:00:00' in fulltime or '12:55:30' in fulltime:
+            print('Created New Contact Tracing List:', fulltime)
+            emailbot.record(fulltime)  
+            time.sleep(1)
+
+elif input1 == 'q' or 'Q':
+    print('Quitting')
+    quit()
+
